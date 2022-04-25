25f9d6d223155befb6f1d7499515280ba1bda9de
saipriya9 <saipriya.java8@gmail.com>
2022-04-25 13:37:07 +0530

    commit

--- src/main/java/in/ashokit/service/TicketService.java
+++ src/main/java/in/ashokit/service/TicketService.java
@@ -1,6 +1,5 @@
 package in.ashokit.service;
 
-import java.util.List;
 
 import in.ashokit.entity.Passenger;
 import in.ashokit.entity.Ticket;
@@ -8,5 +7,5 @@ import in.ashokit.entity.Ticket;
 public interface TicketService {
 public Ticket bookTicket(Passenger passenger);
 public Ticket getTicket(String ticketNum);
-public List<Ticket> getAllTickets(); 
+
 }
